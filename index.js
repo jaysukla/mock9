9bd4aae34d071535bf040b2765bb7c42bc358bec
jaysukla <am7343249@gmail.com>
2023-06-06 14:06:05 +0530

    v3

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ app.get('/',(req,res)=>{
 //signup
 
 app.post('/api/register', async (req, res) => {
-    let {email,password}=req.body;
+    let {name,email,password,dob,bio}=req.body;
       bcrypt.hash(password, 5, async function(err, hash) {
-     let D = await User.insertMany([{email,'password':hash}])
+     let D = await User.insertMany([{name,email,'password':hash,dob,bio}])
     
     });
     
